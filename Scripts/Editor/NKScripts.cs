69ba2ac9e642413ef90e819e2ba874bcbbe6f99c
Your Name <you@example.com>
2022-04-02 15:11:22 -0300

    Script creation and default settings

---
+++ Scripts/Editor/NKScripts.cs
@@ -0,0 +1,83 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System;
+
+namespace NK.ScriptsCreator
+{
+    public class NKScripts
+    {
+        public struct ScriptTemplate
+        {
+            public TextAsset TemplateFile;
+            public string DefaultFileName;
+        }
+
+        private const string DEFAULT_PATH = "Assets/Plugins/NKTools/NKScriptsCreator/Templates/Editor";
+
+        private static readonly ScriptTemplate[] s_ScriptsTemplates =
+        {
+            new ScriptTemplate { TemplateFile = GetTextAssetFile("DefaultMonoBehaviour.cs.txt"), DefaultFileName = "NewMonoBehaviour.cs" },
+            new ScriptTemplate { TemplateFile = GetTextAssetFile("DefaultScriptableObject.cs.txt"), DefaultFileName = "NewScriptableObject.cs" },
+            new ScriptTemplate { TemplateFile = GetTextAssetFile("DefaultClass.cs.txt"), DefaultFileName = "NewDefaultClass.cs" },
+            new ScriptTemplate { TemplateFile = GetTextAssetFile("DefaultStruct.cs.txt"), DefaultFileName = "NewDefaultStruct.cs" },
+            new ScriptTemplate { TemplateFile = GetTextAssetFile("DefaultInterface.cs.txt"), DefaultFileName = "NewDefaultInterface.cs" }
+        };
+
+        private static TextAsset GetTextAssetFile(string templateFileName)
+        {
+            string path = Path.Combine(DEFAULT_PATH, templateFileName);
+            return AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+        }
+
+        private static void CreateNewScript(ScriptTemplate template)
+        {
+            string templatePath = AssetDatabase.GetAssetPath(template.TemplateFile);
+
+            string[] lines = template.TemplateFile.text.Split('\n');
+            string definedNamespace = NKScriptsCreatorSettings.GetOrCreateSettings().Namespace;
+            if (!lines[2].Contains(definedNamespace))
+            {
+                lines[2] = $"namespace {definedNamespace}\n";
+                string final = string.Empty;
+                Array.ForEach(lines, line => final += line);
+                File.WriteAllText(templatePath, final);
+                AssetDatabase.Refresh();
+            }
+
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(templatePath, template.DefaultFileName);
+        }
+
+        [MenuItem("Assets/Create/Scripts/New MonoBehaviour", priority = 52)]
+        public static void CreateMonoBehaviour()
+        {
+            CreateNewScript(s_ScriptsTemplates[0]);
+        }
+
+        [MenuItem("Assets/Create/Scripts/New ScriptableObject")]
+        public static void CreateScriptableObject()
+        {
+            CreateNewScript(s_ScriptsTemplates[1]);
+        }
+
+        [MenuItem("Assets/Create/Scripts/New Class")]
+        public static void CreateDefaultClass()
+        {
+            CreateNewScript(s_ScriptsTemplates[2]);
+        }
+
+        [MenuItem("Assets/Create/Scripts/New Struct")]
+        public static void CreateDefaultStruct()
+        {
+            CreateNewScript(s_ScriptsTemplates[3]);
+        }
+
+        [MenuItem("Assets/Create/Scripts/New Interface")]
+        public static void CreateDefaultInterface()
+        {
+            CreateNewScript(s_ScriptsTemplates[4]);
+        }
+    }
+}
+#endif
---
+++ Scripts/Editor/NKScriptsCreatorSettings.cs
@@ -0,0 +1,88 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace NK.ScriptsCreator
+{
+    public class NKScriptsCreatorSettings : ScriptableObject
+    {
+        public string Namespace = "NK";
+
+        private static NKScriptsCreatorSettings FindSettings()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:NKScriptsCreatorSettings");
+            if (guids.Length > 1) Debug.LogWarning("Found multiple settings files, using the first.");
+
+            switch (guids.Length)
+            {
+                case 0:
+                    return null;
+                default:
+                    string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                    return AssetDatabase.LoadAssetAtPath<NKScriptsCreatorSettings>(path);
+            }
+        }
+
+        public static NKScriptsCreatorSettings GetOrCreateSettings()
+        {
+            NKScriptsCreatorSettings settings = FindSettings();
+            if (settings == null)
+            {
+                settings = CreateInstance<NKScriptsCreatorSettings>();
+                AssetDatabase.CreateAsset(settings, "Assets/Plugins/NKTools/NKScriptsCreator/Settings/Editor/NKScriptsCreatorSettings.asset");
+                AssetDatabase.SaveAssets();
+            }
+            return settings;
+        }
+
+        public static SerializedObject GetSerializedSettings()
+        {
+            return new SerializedObject(GetOrCreateSettings());
+        }
+    }
+
+    // Register a SettingsProvider using UIElements for the drawing framework:
+    public static class NKScriptsCreatorSettingsCustomUI
+    {
+        [SettingsProvider]
+        public static SettingsProvider CreateMyCustomSettingsProvider()
+        {
+            // First parameter is the path in the Settings window.
+            // Second parameter is the scope of this setting: it only appears in the Settings window for the Project scope.
+            var provider = new SettingsProvider("Project/NKTools/NKScriptsCreator", SettingsScope.Project)
+            {
+                label = "NK Scripts Creator",
+                // activateHandler is called when the user clicks on the Settings item in the Settings window.
+                activateHandler = (searchContext, rootElement) =>
+                {
+                    SerializedObject settings = NKScriptsCreatorSettings.GetSerializedSettings();
+
+                    // rootElement is a VisualElement. If you add any children to it, the OnGUI function
+                    // isn't called because the SettingsProvider uses the UIElements drawing framework.
+                    var title = new Label()
+                    {
+                        text = "NK Scripts Creator"
+                    };
+                    title.AddToClassList("title");
+                    rootElement.Add(title);
+
+                    var properties = new VisualElement()
+                    {
+                        style = { flexDirection = FlexDirection.Column }
+                    };
+                    properties.AddToClassList("property-list");
+                    rootElement.Add(properties);
+
+                    properties.Add(new InspectorElement(settings));
+
+                    rootElement.Bind(settings);
+                },
+            };
+
+            return provider;
+        }
+    }
+}
+#endif
